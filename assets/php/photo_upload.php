c3076d5af93ccfd2ea418b1c75842e1afe9f6a20
Matt Hickling <m.hickling@hotmail.co.uk>
2023-04-21 16:25:13 +0100

    bootstrap file

--- assets/php/photo_upload.php
+++ assets/php/photo_upload.php
@@ -6,23 +6,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $file_size = $file['size'];
     $file_tmp_name = $file['tmp_name'];
     $file_error = $file['error'];
-    $file_type = mime_content_type($file_tmp_name); // requires PHP 5.3 or higher
+    $file_type = mime_content_type($file_tmp_name); 
 
-    // Check for errors
+
     if ($file_error !== UPLOAD_ERR_OK) {
         die('Error uploading file');
     }
 
-    // Check file type
+
     if ($file_type !== 'image/jpeg' && $file_type !== 'image/png') {
         die('Invalid file type. Only JPEG and PNG files are allowed.');
     }
 
-    // Move uploaded file to destination folder
+
     $destination = 'assets/uploads/' . $file_name;
     move_uploaded_file($file_tmp_name, $destination);
 
-    // Return uploaded image URL and name as JSON response
+  
     $response = array(
         'url' => $destination,
         'name' => $file_name
